ba2f788762e3d1c1652a0b1a2421355b09453a00
chungskim90-ux <chungskim90@gmail.com>
2026-08-21 14:19:50 -0400

    Create feeds.py

---
+++ backend/feeds.py
@@ -0,0 +1,81 @@
+RSS_FEEDS = [
+    # Fantasy news
+    "https://www.fantasypros.com/rss/nfl-news.xml",
+    "https://www.rotowire.com/rss/news.php?sport=nfl",
+
+    # National news
+    "https://profootballtalk.nbcsports.com/feed/",
+    "https://www.nbcsports.com/rss/nfl",
+    "https://www.nfl.com/rss/rsslanding?searchString=news",
+
+    # AFC East — SB Nation
+    "https://www.buffalorumblings.com/rss/index.xml",
+    "https://www.thephinsider.com/rss/index.xml",
+    "https://www.patspulpit.com/rss/index.xml",
+    "https://www.ganggreennation.com/rss/index.xml",
+
+    # AFC North — SB Nation
+    "https://www.baltimorebeatdown.com/rss/index.xml",
+    "https://www.behindthesteelcurtain.com/rss/index.xml",
+
+
+    # AFC South — SB Nation
+    "https://www.battleredblog.com/rss/index.xml",
+    "https://www.stampedeblue.com/rss/index.xml",
+    "https://www.bigcatcountry.com/rss/index.xml",
+    "https://www.musiccitymiracles.com/rss/index.xml",
+
+
+    # AFC West — SB Nation
+    "https://www.milehighreport.com/rss/index.xml",
+    "https://www.arrowheadpride.com/rss/index.xml",
+    "https://www.silverandblackpride.com/rss/index.xml",
+    "https://www.boltsfromtheblue.com/rss/index.xml",
+
+    # AFC West — Beat Writers
+    "https://nitter.net/MikeKlis/rss",
+    "https://nitter.net/ryanohalloran/rss",
+    "https://nitter.net/SamWarrenNFL/rss",
+    "https://nitter.net/VinnyBonsignore/rss",
+    "https://nitter.net/DanielPopper/rss",
+
+    # NFC East — SB Nation
+    "https://www.bloggingtheboys.com/rss/index.xml",
+    "https://www.bigblueview.com/rss/index.xml",
+    "https://www.bleedinggreennation.com/rss/index.xml",
+    "https://www.hogshaven.com/rss/index.xml",
+
+    # NFC North — SB Nation
+    "https://www.windycitygridiron.com/rss/index.xml",
+    "https://www.prideofdetroit.com/rss/index.xml",
+    "https://www.acmepackingcompany.com/rss/index.xml",
+    "https://www.dailynorseman.com/rss/index.xml",
+
+
+    # NFC South — SB Nation
+    "https://www.thefalcoholic.com/rss/index.xml",
+    "https://www.catscratchreader.com/rss/index.xml",
+    "https://www.canalstreetchronicles.com/rss/index.xml",
+    "https://www.bucsnation.com/rss/index.xml",
+
+    # NFC South — Beat Writers
+    "https://nitter.net/JeffSchultzATL/rss",
+    "https://nitter.net/joebucsfan/rss",
+
+    # NFC West — SB Nation
+    "https://www.revengeofthebirds.com/rss/index.xml",
+    "https://www.turfshowtimes.com/rss/index.xml",
+    "https://www.ninersnation.com/rss/index.xml",
+    "https://www.fieldgulls.com/rss/index.xml",
+
+    # Insiders
+    "https://nitter.net/AdamSchefter/rss",
+    "https://nitter.net/RapSheet/rss",
+    "https://nitter.net/TomPelissero/rss",
+    "https://nitter.net/FieldYates/rss",
+    "https://nitter.net/Schultz_Report/rss",
+    "https://nitter.net/AllbrightNFL/rss",
+    "https://nitter.net/ProFootballTalk/rss",
+    "https://nitter.net/ESPNNFL/rss",
+    "https://nitter.net/NFLNetwork/rss",
+]
